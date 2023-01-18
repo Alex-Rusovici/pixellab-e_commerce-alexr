f2b99b7295f002c1aeae825ef9bb2b4202acdb4e
Alexandru Rusovici <alex.rusovici@yahoo.com>
2023-01-18 21:58:58 +0200

    18-01 classwork
    18-01 classwork

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
-import Head from "next/head";
+import { Layout } from '@/layouts';
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hello Athena</main>
+      <Layout>
+        <main>This is main</main>
+      </Layout>
     </>
   );
 }
