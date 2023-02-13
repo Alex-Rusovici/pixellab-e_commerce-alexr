81bd29acf0b18fa23372dee0a9bfdefec969ad4b
Alexandru Rusovici <alex.rusovici@yahoo.com>
2023-02-13 22:10:23 +0200

    13.02 - classwork
    13.02 - classwork

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 export * from './useProducts';
 export * from './useProduct';
-export * from './useLocalStoarge';
+export * from './useLocalStorage';
 export * from './useIsMobile';
 export * from './useCart';
+export * from './useAddToCart';
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,19 +1,17 @@
-import { cache, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-//keyName => key name in local storage
-
+// keyName => key name in local storage
 export const useLocalStorage = (keyName, defaultValue) => {
   const [value, setValue] = useState(defaultValue);
   const [cache, setCache] = useState('');
 
   useEffect(() => {
     console.log('I run once');
-    // get "keyName" from localStorage
+    // incerc sa ma duc in localStorage sa iau "keyName"
     const savedValue = localStorage.getItem(keyName);
 
-    // check if there is a localStorage
-    // if it is in localStorage, add to state
-
+    // verific daca aveam ceva in localStorage
+    // daca aveam ceva, il adaug in state
     if (savedValue !== null) {
       setValue(savedValue);
       setCache(savedValue);
@@ -21,7 +19,7 @@ export const useLocalStorage = (keyName, defaultValue) => {
   }, [setValue, keyName]);
 
   useEffect(() => {
-    // no early return
+    // no early returns
     if (value !== cache) {
       localStorage.setItem(keyName, value);
       setCache(value);
